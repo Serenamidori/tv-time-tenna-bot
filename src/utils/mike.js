be68f111d25824216b39eb191ed367d8b5e742c0
Gabby Gonzalez <serenamidori@gmail.com>
2025-12-02 13:33:07 -0600

    feature: Upgrade to Mike.js to allow for more Mikes

--- src/utils/mike.js
+++ src/utils/mike.js
@@ -1,32 +1,42 @@
 const { AttachmentBuilder, EmbedBuilder } = require("discord.js");
 const path = require('path');
 const random = require('./random');
-const quotes = ["Right away, Mr. Tenna!", "Yous got it, boss.", "..."]
-
-function getMike() {
-  const i = random.rand(3);
-  const mike = {
-    "image": `${i}.png`,
-    "quote": quotes[i]
-  };
-
-  return mike;
-};
+const quotes = [
+  "You got it, Tenna!!",
+  "Comin' right up!!",
+  "Sure thing! Right away!!",
+  "Yous got it, boss.",
+  "...",
+  "...",
+  "..."
+]
 
 async function getPoints(interaction, profile, name) {
-  const i = random.rand(3)-1;
+  const i = randomMikeIndex();
+  const fields = { name: `${name.toUpperCase()}'S BALANCE`, value: `${profile.points} POINTS` }
+  await interaction.channel.send(getMikeMessage(i, null, null, fields));
+}
+
+function randomMikeIndex() {
+  return random.rand(quotes.length) - 1;
+}
+
+function getMikeMessage(i, title = null, description = null, fields = null) {
+  if (title == null) title = `"${quotes[i]}"`;
   const imagePath = path.resolve(__dirname, `../assets/mike/${i}.png`);
   const attachment = new AttachmentBuilder(imagePath, { name: 'Mike.png' });
 
-  const pointsEmbed = new EmbedBuilder()
+  const embed = new EmbedBuilder()
     .setColor(0xFF362C)
-    .setTitle(`"${quotes[i]}"`)
-    .addFields({ name: `${name.toUpperCase()}'S BALANCE`, value: `${profile.points} POINTS` })
+    .setTitle(title)
+    .setDescription(description)
     .setThumbnail(`attachment://${attachment.name}`);
 
-  await interaction.channel.send({ embeds: [pointsEmbed], files: [attachment]});
+  if (fields) embed.addFields(fields);
+
+  return { embeds: [embed], files: [attachment]}
 }
 
 module.exports = {
-  getMike, getPoints
+  randomMikeIndex, getPoints, getMikeMessage
 }
