543cdce7c47e1816fe8abef5b2620f2eeac2bf21
Gab <Serenamidori@gmail.com>
2026-01-20 11:13:44 -0600

    Feat: updated Profile model to include inventory (#15)
    * feat: updated Profile model to include inventory
    * fix: added timestamps for items and profile itself, made images required

--- models/Profile.js
+++ models/Profile.js
@@ -14,7 +14,33 @@ const Profile = new Schema({
     pronouns: String,
     birthday: Date,
     quizCount: Number,
-    lastQuizAt: Date
+    lastQuizAt: Date,
+    inventory: [{
+      name: {
+        type: String, 
+        required: true
+      },
+      description: String,
+      thumbnail: {
+        type: String, 
+        required: true
+      },
+      image: {
+        type: String, 
+        required: true
+      },
+      rarity: {
+        type: String,
+        enum: ['common', 'uncommon', 'rare', 'very rare', 'legendary'],
+        default: 'common'
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    }]
+}, {
+  timestamps: true
 });
 
 module.exports = model('Profile', Profile);
