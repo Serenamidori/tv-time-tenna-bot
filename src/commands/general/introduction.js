796151eb35b06b1e8d4d2e412a8294a49ca21b05
Gabby Gonzalez <serenamidori@gmail.com>
2026-03-01 10:02:21 -0600

    fix: removed uneeded time call

--- src/commands/general/introduction.js
+++ src/commands/general/introduction.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, time } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const { profileService } = require("../../utils");
 
 module.exports = {
